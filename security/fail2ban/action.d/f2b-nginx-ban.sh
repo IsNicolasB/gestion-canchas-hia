321c4781db75d1cc60a872b12021fdcd4be85d29
nicoburgos <isma.nico7842018@gmail.com>
2025-11-28 11:42:59 -0300

    HIA-121 fix: automatic ban

--- security/fail2ban/action.d/f2b-nginx-ban.sh
+++ security/fail2ban/action.d/f2b-nginx-ban.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # f2b-nginx-ban.sh
 # Uso: f2b-nginx-ban.sh add <ip>  | f2b-nginx-ban.sh del <ip>
-# Mantiene /etc/nginx/custom/denied-ips.data
+# Mantiene /etc/nginx/denied-ips.conf y recarga nginx
 
-DENY_FILE="/etc/nginx/custom/denied-ips.data"
+DENY_FILE="/etc/nginx/custom/denied-ips.conf"
 IP="$2"
 
 if [ "$1" = "add" ]; then
-    grep -qx "$IP" "$DENY_FILE" || echo "$IP" >> "$DENY_FILE"
+    grep -q "^$IP " "$DENY_FILE" || echo "$IP 1;" >> "$DENY_FILE"
+#    nginx -s reload
 elif [ "$1" = "del" ]; then
     if [ -f "$DENY_FILE" ]; then
-        grep -vx "$IP" "$DENY_FILE" > "$DENY_FILE.tmp" && mv "$DENY_FILE.tmp" "$DENY_FILE"
+        grep -v "^$IP " "$DENY_FILE" > "$DENY_FILE.tmp" && mv "$DENY_FILE.tmp" "$DENY_FILE"
+        #nginx -s reload
     fi
 fi
-
-# ModSecurity NO necesita reload
